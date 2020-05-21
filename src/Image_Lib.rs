8ed8d1e3b6d99fee7e5b8a456ea9782171fe5166
Kriti Tulsian <kriti.tulsian96@gmail.com>
2020-05-21 20:52:37 +0800

    Update Image_Lib.rs

--- src/Image_Lib.rs
+++ src/Image_Lib.rs
@@ -1,8 +1,6 @@
 use image::imageops::FilterType;
 use std::path::Path;
-
 use std::env;
-
 
 fn main() {
         let args: Vec<String> = env::args().collect();
@@ -18,24 +16,15 @@ fn open_scale_image(filename: String){
     let input_file =  Path::new("images/original").join(&filename);
     let img = image::open(input_file).unwrap();
     
-    let scaled = img.resize(299, 299, FilterType::CatmullRom);
+    let scaled = img.resize_exact(299, 299,FilterType:: CatmullRom);
    
     let file = output_file(filename); //to get the filename 
     let path = Path::new("images/resized").join(file);
     scaled.save(path).unwrap();
 }
 
-//extracting the filename without extension from the filename given by the user
 fn output_file (filename: String) ->String{
 
-        let mut file_name: Vec<String> = Vec::new();
-        for character in filename.chars(){
-                match character {
-                '.' => break,
-                _ => file_name.push(character.to_string()),
-             };
-        }
-             file_name.push("_resized.jpg".to_string()); //appending _resized to the output filename
-             return file_name.join("")
+        format!("{}{}", filename, "_resized.jpg".to_string())  //TODO - sort out ugly naming convention
 }
 
