d322f1bbab0b0945505f06a67908408b30a7a113
Kriti Tulsian <kriti.tulsian96@gmail.com>
2020-05-21 18:56:41 +0800

    updated with chnages for issue #1
    Updated with the changes for issue #1

--- src/Image_Lib.rs
+++ src/Image_Lib.rs
@@ -10,12 +10,32 @@ fn main() {
                 panic!("not enough arguments please specify the file name");
         }
         let filename = &args[1]; 
-        open_scale_image(filename.to_string());
-      
+        open_scale_image(filename.to_string());     
 }
-pub fn open_scale_image(filename: String){
-    let img = image::open(filename).unwrap();
+
+fn open_scale_image(filename: String){
+
+    let input_file =  Path::new("images/original").join(&filename);
+    let img = image::open(input_file).unwrap();
+    
     let scaled = img.resize(299, 299, FilterType::CatmullRom);
-    let path = Path::new("image_resizes.jpg");
+   
+    let file = output_file(filename); //to get the filename 
+    let path = Path::new("images/resized").join(file);
     scaled.save(path).unwrap();
 }
+
+//extracting the filename without extension from the filename given by the user
+fn output_file (filename: String) ->String{
+
+        let mut file_name: Vec<String> = Vec::new();
+        for character in filename.chars(){
+                match character {
+                '.' => break,
+                _ => file_name.push(character.to_string()),
+             };
+        }
+             file_name.push("_resized.jpg".to_string()); //appending _resized to the output filename
+             return file_name.join("")
+}
+
