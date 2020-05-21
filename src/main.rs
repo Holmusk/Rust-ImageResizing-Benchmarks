79818ba6a4e0c23c0baea6cc292a22c01b8abf8d
Kriti Tulsian <kriti.tulsian96@gmail.com>
2020-05-21 15:22:55 +0800

    resizing images with Image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use image::imageops::FilterType;
+use std::path::Path;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+        let args: Vec<String> = env::args().collect();
+        if args.len() < 2 {
+                panic!("not enough arguments please specify the file name");
+        }
+        let filename = &args[1]; 
+        let img = image::open(filename).unwrap();
+        let scaled = img.resize(299, 299, FilterType::CatmullRom); 
+        let path = Path::new("image_resized.jpg");
+        scaled.save(path).unwrap();
 }
+
