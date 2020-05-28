ad4eb8dfa21fdcf724d79c05912248ac70969058
Kriti Tulsian <kriti.tulsian96@gmail.com>
2020-05-28 14:13:59 +0800

    Added fn to upload resized img to S3

--- src/img_resizing_lambda.rs
+++ src/img_resizing_lambda.rs
@@ -1,6 +1,6 @@
 use futures::TryStreamExt;
 use image::imageops::FilterType;
-use rusoto_core::Region;
+use rusoto_core::{Region,ByteStream};
 use rusoto_s3::S3Client;
 use rusoto_s3::S3;
 
@@ -8,20 +8,19 @@ const BUCKET_NAME: &str = "rust-image-resizing";
 const IMAGE_NAME: &str = "rust-image.jpg";
 const REGION_NAME: &str = "ApSoutheast1";
 
-fn main() {
+ fn main() {
     println!("Downloading file from S3 bucket...");
     let awsregion = get_region(REGION_NAME.to_string());
     let s3 = S3Client::new(awsregion);
     download_img_from_s3(s3, BUCKET_NAME.to_string(), IMAGE_NAME.to_string());
 }
 
-fn get_region(aws_region_name : String) -> Region{
-  
-        match aws_region_name.parse::<Region>() {
-            Ok(valid_region) =>  valid_region,
-            // Default fallback Region (Singapore)
-            Err(_) =>  Region::ApSoutheast1,
-        }
+fn get_region(aws_region_name: String) -> Region {
+    match aws_region_name.parse::<Region>() {
+        Ok(valid_region) => valid_region,
+        // Default fallback Region (Singapore)
+        Err(_) => Region::ApSoutheast1,
+    }
 }
 
 async fn download_img_from_s3(
@@ -56,11 +55,11 @@ async fn download_img_from_s3(
     let bytes_mutref = s3_object_bytes_mut.as_ref();
 
     let resized_image = resize_image(bytes_mutref);
-}
     
+     upload_resized_img_to_s3(s3_client, BUCKET_NAME.to_string(), IMAGE_NAME.to_string(),resized_image).await;
+}
 
- fn resize_image(bytes_img: &[u8]) -> Vec<u8> {
-
+fn resize_image(bytes_img: &[u8]) -> Vec<u8> {
     let mut img_result: Vec<u8> = Vec::new();
 
     let image = match image::load_from_memory(bytes_img) {
@@ -69,12 +68,29 @@ async fn download_img_from_s3(
     };
     let scaled = image.resize_exact(299, 299, FilterType::CatmullRom);
 
-    match scaled.write_to(&mut img_result, image::ImageOutputFormat::Jpeg(90)){   //setting the jpeg quality to 90
+    match scaled.write_to(&mut img_result, image::ImageOutputFormat::Jpeg(90)) {
+        //setting the jpeg quality to 90
         Ok(scaledimg) => scaledimg,
         Err(write_err) => panic!("Couldn't convert S3 Object to Image Bytes! {}", write_err),
     }
-    return img_result 
+    return img_result;
 }
 
 
-
+async fn upload_resized_img_to_s3(
+    s3_client: rusoto_s3::S3Client,
+    bucket_name: String,
+    img_name: String,
+    body: Vec<u8>,
+) {  
+    match s3_client.put_object(rusoto_s3::PutObjectRequest {
+        bucket:bucket_name,
+        key: img_name.clone(),
+        body: Some(ByteStream::from(body)),
+        ..Default::default()
+    }).await{
+        Ok(img) =>img,
+        Err(s3_geterr) => panic!("Couldn't PUT image to S3! {}", s3_geterr),
+    };
+  
+}
